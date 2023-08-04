893f33124fd1687ebabb5a90d746dbcc54d9eebc
3than <3thanfaso03@gmail.com>
2023-08-04 08:27:43 -0600

    home

--- Conectopia/social_media/home/models.py
+++ Conectopia/social_media/home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Publicacion(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='publicaciones/', null=True, blank=True)
--- Conectopia/social_media/home/views.py
+++ Conectopia/social_media/home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from usuarios.models import Usuarios
+from home.models import Publicacion
 
 import json
 from bson import ObjectId
@@ -16,3 +17,16 @@ def visit_home(request):
     preferencesRecommendation = json.loads(request.session['preferenceSuggestion'])
     
     return render(request, 'home.html',{'user': user,'friends':friendsRecommendation, 'preferenceRecommendations':preferencesRecommendation})
+
+
+#publicaciones
+def home_view(request):
+    publicaciones = Publicacion.objects.all()  # Obtiene todas las publicaciones
+    return render(request, 'home.html', {'publicaciones': publicaciones})
+
+
+def delete_post(request, Publicacion_id):
+    Publicacion = Publicacion.objects.get(pk=Publicacion_id)
+    Publicacion.delete()
+    return redirect('home_view')  
+
